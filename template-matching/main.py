90ca352c807e58dcb55e24b244c096a686b7e6f3
Irineu de Almeida Júnior <66287053+IrineuAlmeidaJr@users.noreply.github.com>
2023-06-22 16:55:01 -0300

    Organização de pastas

--- template-matching/main.py
+++ template-matching/main.py
@@ -41,11 +41,20 @@ def fast_brief(query_img, train_img):
     matches = matcher.match(des1, des2)
     matches = sorted(matches, key=lambda x: x.distance)
 
+    # Definir um limiar de distância para filtrar correspondências ruins
+    threshold_distance = 20
+
+    # Filtrar correspondências com base no limiar de distância
+    good_matches = []
+    for match in matches:
+        if match.distance < threshold_distance:
+            good_matches.append(match)
+
     fim = datetime.now()
 
     tempo_gasto = fim - inicio
 
-    inliers, outliers, img_difference = ransac(img1, img2, kp1, kp2, matches)
+    inliers, outliers, img_difference = ransac(img1, img2, kp1, kp2, good_matches)
 
     return inliers, outliers, img_difference, tempo_gasto
 
@@ -157,12 +166,6 @@ def sift_sift(query_img, train_img):
     kp1, des1 = sift.detectAndCompute(img1, None)
     kp2, des2 = sift.detectAndCompute(img2, None)
 
-    # --> Desenha Ponto Chaves
-    # img1_sift = cv2.drawKeypoints(img1, kp1, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    # cv2.imshow('SIFT img1', img1_sift)
-    # img2_sift = cv2.drawKeypoints(img2, kp2, None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    # cv2.imshow('SIFT img2', img2_sift)
-
     # --> Faz Correspondência
     # Correspondência
     FLANN_INDEX_KDTREE = 0
@@ -237,22 +240,25 @@ def equaliza_cor(sat, uav):
     sat = cv2.cvtColor(sat, cv2.COLOR_BGR2LAB)
     uav = cv2.cvtColor(uav, cv2.COLOR_BGR2LAB)
 
-    # Calcular a média e o desvio padrão dos canais L, a e b da imagem img1
+    # Calcular a média e o desvio padrão dos canais L, a e b da imagem SAT
     media_pixels_sat, desvio_padrao_sat = cv2.meanStdDev(sat)
-    media_pixels_sat = media_pixels_sat.ravel()  # achatada em um array 1D, que pode ser mais facilmente manipulado ou processado
-    desvio_padrao_sat = desvio_padrao_sat.ravel()  # achatada em um array 1D, que pode ser mais facilmente manipulado ou processado
+    # achatada em um array 1D, que pode ser mais facilmente manipulado
+    media_pixels_sat = media_pixels_sat.ravel()
+    desvio_padrao_sat = desvio_padrao_sat.ravel()
 
-    # Normalizar os canais da imagem img2 pela média e desvio padrão da imagem img1
-    uav[:, :, 0] = np.clip(((uav[:, :, 0] - np.mean(uav[:, :, 0])) * (desvio_padrao_sat[0] / np.std(uav[:, :, 0]))) \
+    # Calcular a média e o desvio padrão dos canais L, a e b da imagem SAT
+    media_pixels_uav, desvio_padrao_uav = cv2.meanStdDev(sat)
+    # achatada em um array 1D, que pode ser mais facilmente manipulado
+    media_pixels_uav = media_pixels_uav.ravel()
+    desvio_padrao_uav = desvio_padrao_uav.ravel()
+
+    # Normalizar os canais da imagem UAV pela média e desvio padrão da imagem SAT
+    uav[:, :, 0] = np.clip((desvio_padrao_sat[0] / desvio_padrao_uav[0]) * (uav[:, :, 0] - media_pixels_uav[0])
                            + media_pixels_sat[0], 0, 255)
-    # esta tendo que normalizar, não entendi muito o porque seguindo o artigo eu não conseguir, perguntar
-    # Teve que normalizar
-    # img2[:, :, 0] = ((img2[:, :, 0] - np.mean(img2[:, :, 0])) * (desvio_padrao_img1[0] / np.std(img2[:, :, 0]))) \
-    #                 + media_pixels_img1[0]
-    uav[:, :, 1] = ((uav[:, :, 1] - np.mean(uav[:, :, 1])) * (desvio_padrao_sat[1] / np.std(uav[:, :, 1]))) \
-                   + media_pixels_sat[1]
-    uav[:, :, 2] = ((uav[:, :, 2] - np.mean(uav[:, :, 2])) * (desvio_padrao_sat[2] / np.std(uav[:, :, 2]))) \
-                   + media_pixels_sat[2]
+    uav[:, :, 1] = np.clip((desvio_padrao_sat[1] / desvio_padrao_uav[1]) * (uav[:, :, 1] - media_pixels_uav[1])
+                   + media_pixels_sat[1], 0, 255)
+    uav[:, :, 2] = np.clip((desvio_padrao_sat[2] / desvio_padrao_uav[2]) * (uav[:, :, 2] - media_pixels_uav[2])
+                   + media_pixels_sat[2], 0, 255)
 
     # Converter de volta para o espaço de cor BGR
     result = cv2.cvtColor(uav, cv2.COLOR_LAB2BGR)
@@ -261,7 +267,6 @@ def equaliza_cor(sat, uav):
 
     return result
 
-
 def calcula_exibe_histograma(img, titulo):
     hist = cv2.calcHist([img], [0], None, [256], [0, 256])
     plt.plot(hist)
@@ -269,64 +274,6 @@ def calcula_exibe_histograma(img, titulo):
     plt.xlabel('Intensidade de pixel')
     plt.ylabel('Contagem')
     plt.show()
-
-
-# def main():
-#     # query_img = cv2.imread('./data/outros/lena.png')
-#     # train_img = cv2.imread('./data/outros/lena1.jpg')
-#     # query_img = cv2.imread('./data/curitiba_1/curitiba_1_04-20.png')
-#     # train_img = cv2.imread('./data/curitiba_1/curitiba_1_05-21_angulo.png')
-#     # query_img = cv2.imread('./data/curitiba_2/curitiba_2_04-20.jpg')
-#     # train_img = cv2.imread('./data/curitiba_2/curitiba_2_05-21.jpg')
-#     # query_img = cv2.imread('./data/machado_1/machado_1_3-20.jpg')
-#     # train_img = cv2.imread('./data/machado_1/machado_1_8-22.png')
-#
-#     # query_img = cv2.imread('./data/curitiba_2/curitiba_2_04-20.jpg')
-#     # train_img = cv2.imread('./data/curitiba_2/curitiba_2_05-21.jpg')
-#
-#     # # -> Drone
-#     # query_img = cv2.imread('./database/alvares_machado/06-22/alvares_machado_08.jpg')
-#     # # -> Satélite
-#     # train_img = cv2.imread('./database/alvares_machado/04-23/alvares_machado_08.jpg')
-#
-#     query_img = cv2.imread('./database/curitiba/04-20/curitiba_100.jpg')
-#     train_img = cv2.imread('./database/curitiba/05-21/curitiba_100.jpg')
-#
-#     # ----> IMAGEM ORIGINAIS
-#     # cv2.imshow("satelite", query_img)
-#     # cv2.imshow("drone - original", train_img)
-#     cv2.imshow("drone - original", query_img)
-#     cv2.imshow("satelite", train_img)
-#
-#     # ----> IMAGEM EQUALIZADA
-#     # train_img = equaliza_cor(query_img, train_img)
-#     query_img = equaliza_cor(train_img, query_img)
-#     cv2.imshow("drone - equalizada", query_img)
-#
-#     # histograma(query_img, train_img)
-#
-#     # orb(query_img, train_img)
-#     # sift(query_img, train_img)
-#     # sif_match_ransac(query_img, train_img)
-#
-#     # inliers, outliers, img_difference, img_show, tempo = sift_sift(query_img, train_img)
-#     # inliers, outliers, img_difference, img_show, tempo = orb_orb(query_img, train_img)
-#     inliers, outliers, img_difference, img_show, tempo = akaze_akaze(query_img, train_img)
-#     # inliers, outliers, img_difference, img_show, tempo = fast_brief(query_img, train_img)
-#     # inliers, outliers, img_difference, img_show, tempo = fast_sift(query_img, train_img)
-#
-#     print("Número de inliers: ", inliers)
-#     print("Número de outliers: ", outliers)
-#     print("Subtração (img2-img1): ", img_difference)
-#     print("Tempo: ", tempo)
-#
-#     # ----> IMAGEM COM CORRESPONDÊNCIA
-#     cv2.imshow("Matches-img", img_show)
-#
-#     # remove_areas_verdes(train_img)
-#
-#     cv2.waitKey(0)
-
 
 def criar_arquivo():
     caminho = 'resultado/'
@@ -373,7 +320,6 @@ def main():
 
         # -> ROTACIONAR IMAGEM
         img_anterior_45 = rotacionar_imagem(img_anterior, 45)
-        img_atual_45 = rotacionar_imagem(img_atual, 45)
 
         # ---------------------- ALGORITMOS ----------------------
         # -> SIFT_SIFT
@@ -381,35 +327,35 @@ def main():
             sift_sift(img_anterior, img_atual)
         # -> SIFT_SIFT_45
         SIFT_SIFT_45_Inliers, SIFT_SIFT_45_Outliers, SIFT_SIFT_45_Diferenca, SIFT_SIFT_45_Tempo = \
-            sift_sift(img_anterior_45, img_atual_45)
+            sift_sift(img_anterior_45, img_atual)
         # ----------------------
         # -> ORB_ORB
         ORB_ORB_Inliers, ORB_ORB_Outliers, ORB_ORB_Diferenca, ORB_ORB_Tempo = \
             orb_orb(img_anterior, img_atual)
         # -> ORB_ORB_45
         ORB_ORB_45_Inliers, ORB_ORB_45_Outliers, ORB_ORB_45_Diferenca, ORB_ORB_45_Tempo = \
-            orb_orb(img_anterior_45, img_atual_45)
+            orb_orb(img_anterior_45, img_atual)
         # ----------------------
         # -> AKAZE_AKAZE
         AKAZE_AKAZE_Inliers, AKAZE_AKAZE_Outliers, AKAZE_AKAZE_Diferenca, AKAZE_AKAZE_Tempo = \
             akaze_akaze(img_anterior, img_atual)
         # -> AKAZE_AKAZE_45
         AKAZE_AKAZE_45_Inliers, AKAZE_AKAZE_45_Outliers, AKAZE_AKAZE_45_Diferenca, AKAZE_AKAZE_45_Tempo = \
-            akaze_akaze(img_anterior_45, img_atual_45)
+            akaze_akaze(img_anterior_45, img_atual)
         # ----------------------
         # -> FAST_BRIEF
         FAST_BRIEF_Inliers, FAST_BRIEF_Outliers, FAST_BRIEF_Diferenca, FAST_BRIEF_Tempo = \
             fast_brief(img_anterior, img_atual)
         # -> FAST_BRIEF_45
         FAST_BRIEF_45_Inliers, FAST_BRIEF_45_Outliers, FAST_BRIEF_45_Diferenca, FAST_BRIEF_45_Tempo = \
-            fast_brief(img_anterior_45, img_atual_45)
+            fast_brief(img_anterior_45, img_atual)
         # ----------------------
         # -> FAST_SIFT
         FAST_SIFT_Inliers, FAST_SIFT_Outliers, FAST_SIFT_Diferenca, FAST_SIFT_Tempo = \
             fast_sift(img_anterior, img_atual)
         # -> FAST_SIFT_45
         FAST_SIFT_45_Inliers, FAST_SIFT_45_Outliers, FAST_SIFT_45_Diferenca, FAST_SIFT_45_Tempo = \
-            fast_sift(img_anterior_45, img_atual_45)
+            fast_sift(img_anterior_45, img_atual)
 
 
         escrever_linha_arquivo(f"\n{num_arq};{SIFT_SIFT_Inliers};{SIFT_SIFT_Outliers};{SIFT_SIFT_Diferenca};{converter_tempo(SIFT_SIFT_Tempo)};"
